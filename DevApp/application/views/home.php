87c80710e205e15bf9498b58160f2003871c480d
Hanss121 <kangjihan121@gmail.com>
2023-06-08 05:03:28 +0700

    navbar

--- DevApp/application/views/home.php
+++ DevApp/application/views/home.php
@@ -11,10 +11,10 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 <body>
     <!-- Navbar star -->
-    <nav class="mt-10">
+    <nav class="mt-4 ml-2 mr-2">
         <div class="sm:flex justify-around border rounded-md shadow-md">
             <div class="flex justify-between">
-                <h1 class="bg-slate-500 p-5">LOGO</h1>
+                <h1 class=" p-5">LOGO</h1>
                 <div class="flex items-center relative px-4 sm:hidden">
                     <button class="" id="tombolMenu" name="tombolMenu" type="button">
                         <span class="tombol-line transition duration-300 ease-in-out origin-top-left"></span>
@@ -23,12 +23,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
                     </button>
                 </div>
             </div>
-            <div id="hilang" nama="hilang" class="sm:flex hidden">
-                <ul class="sm:flex ">
-                    <li class="items-center p-5"><a class="items-center" href="#">HOME</a></li>
-                    <li class="p-5"><a href="#">PRODUK</a></li>
-                    <li class="p-5"><a href="">TRANSAKSI</a></li>
-                    <li class="p-5"><a href="">KONTAK</a></li>
+            <div class="p-3" id="hilang" nama="hilang" class="sm:flex hidden">
+                <ul class="sm:flex sm:border-none sm:rounded-none sm:shadow-none border rounded-md shadow-md ">
+                    <li class="text-center sm:p-4 p-1"><a class="" href="#">HOME</a></li>
+                    <li class="text-center sm:p-4 p-1"><a href="#">PRODUK</a></li>
+                    <li class="text-center sm:p-4 p-1"><a href="">TRANSAKSI</a></li>
+                    <li class="text-center sm:p-4 p-1"><a href="">KONTAK</a></li>
                 </ul>
             </div>
         </div>
