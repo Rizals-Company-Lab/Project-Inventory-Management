3dae35cb42ede23b5b7132cae4daafb254fc3079
Rizalfaizin06 <rizalfaizin06@gmail.com>
2023-07-25 21:17:03 +0700

    tambah report clean

---
+++ DevApp/application/controllers/Report.php
@@ -0,0 +1,80 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Report extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Report_model');
+        $this->load->model('Product_model');
+    }
+
+    public function index($row_no = 0)
+    {
+        //search text
+        $searchINVENTTRANSID = "";
+        $searchNAME = "";
+        $searchITEMNAME = "";
+        if ($this->input->post('search') != '') {
+            $searchINVENTTRANSID = $this->input->post('searchINVENTTRANSID');
+            $searchNAME = $this->input->post('searchNAME');
+            $searchITEMNAME = $this->input->post('searchITEMNAME');
+            $this->session->set_userdata("searchINVENTTRANSID", $searchINVENTTRANSID);
+            $this->session->set_userdata("searchNAME", $searchNAME);
+            $this->session->set_userdata("searchITEMNAME", $searchITEMNAME);
+        } else {
+            if ($this->session->userdata('searchINVENTTRANSID') != "") {
+                $searchINVENTTRANSID = $this->session->userdata('searchINVENTTRANSID');
+            }
+            if ($this->session->userdata('searchNAME') != "") {
+                $searchNAME = $this->session->userdata('searchNAME');
+            }
+            if ($this->session->userdata('searchITEMNAME') != "") {
+                $searchITEMNAME = $this->session->userdata('searchITEMNAME');
+            }
+        }
+
+        //--pagination--
+        $row_per_page = 5;
+
+        if ($row_no != 0) {
+            $row_no = ($row_no - 1) * $row_per_page;
+        }
+
+        // Pagination Configuration
+        // All record count
+        $config['total_rows'] = $this->Report_model->get_report_count_byProduct($searchINVENTTRANSID, $searchNAME, $searchITEMNAME);
+        $config['base_url'] = base_url() . 'report/index';
+        $config['use_page_numbers'] = true;
+        $config['per_page'] = $row_per_page;
+
+        //initialize
+        $this->pagination->initialize($config);
+
+        $data['pagination'] = $this->pagination->create_links();
+
+        // Get record
+        $data['report'] = $this->Report_model->get_report_byProduct($row_no, $row_per_page, $searchINVENTTRANSID, $searchNAME, $searchITEMNAME);
+        $data['grandTotal'] = $this->Report_model->get_report();
+
+        $data['row'] = $row_no;
+
+        $data['searchINVENTTRANSID'] = $searchINVENTTRANSID;
+        $data['searchNAME'] = $searchNAME;
+        $data['searchITEMNAME'] = $searchITEMNAME;
+        $data['totalRow'] = $config['total_rows'];
+
+
+        // var_dump($this->Report_model->get_report());
+
+        $this->load->view('report/report_view.php', $data);
+        // $this->load->view('report/report_view', $data);
+    }
+
+
+
+
+
+}
